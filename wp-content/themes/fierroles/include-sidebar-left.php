dacd0e088c77213767f1b8c7b11cb4a1863b2591
Gus <yo@soygus.com>
2015-03-24 11:34:39 -0500

    Main menu

--- wp-content/themes/fierroles/include-sidebar-left.php
+++ wp-content/themes/fierroles/include-sidebar-left.php
@@ -1,22 +1,22 @@
 <aside class="sidebar-left">
   <div class="text-right">
     <h1 class="h4 uppercase text-gray-dark lintel-bold m-0"><a href="#">Carlos Fierro</a></h1>
-    <ul class="list-unstyled uppercase lintel-regular pv-7">
+    <!-- <ul class="list-unstyled uppercase lintel-regular pv-7"> -->
       <!-- <li><a href="#">Works</a></li>
       <li><a href="#">Bio</a></li>
       <li><a href="#">Media</a></li>
       <li><a href="#">Contact</a></li> -->
-      <?php 
+    <?php 
 
-        $defaults = array(
-          'container' => false,
-          'theme_location' => 'main-menu'
-          // 'menu_class' => 'ul-class'
-          );
-        wp_nav_menu ($defaults);
-        
-      ?>
-    </ul>
+      $defaults = array(
+        'container' => false,
+        'theme_location' => 'main-menu'
+        'menu_class' => 'list-unstyled uppercase lintel-regular pv-7'
+        );
+      wp_nav_menu ($defaults);
+      
+    ?>
+    <!-- </ul> -->
     <ul class="nav-project-grid-filters mv-42">
       <li><a href="#" class="* all active" data-filter="*">All</a></li>
       <li><a href="#" class="spaces" data-filter=".spaces">Spaces</a></li>
