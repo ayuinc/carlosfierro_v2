2a3b2e6eef814037987336cd3a3d2ca1f4c23682
Gus <yo@soygus.com>
2015-03-24 14:56:20 -0500

    Project images

--- wp-content/themes/fierroles/single-works.php
+++ wp-content/themes/fierroles/single-works.php
@@ -7,20 +7,18 @@
 <?php get_header(); ?>
   <?php get_template_part( 'include', 'sidebar-left' ); ?>
   <main class="main-content works-padding" role="main">
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <div class="gallery-container">
       <div class="gallery js-flickity" data-flickity-options='{ "cellAlign": "left", "contain": true, "pageDots": false }'>
-        <div class="gallery-cell">
-          <img src="assets/img/img.png" alt="Image" class="ignore-srcset">
-        </div>
-        <div class="gallery-cell">
-          <img src="assets/img/img.png" alt="Image" class="ignore-srcset">
-        </div>
-        <div class="gallery-cell">
-          <img src="assets/img/img.png" alt="Image" class="ignore-srcset">
-        </div>
-        <div class="gallery-cell">
-          <img src="assets/img/img.png" alt="Image" class="ignore-srcset">
-        </div>
+        <?php 
+          $images = get_field('gallery');
+          if( $images ): ?>
+          <?php foreach( $images as $image ): ?>
+            <div class="gallery-cell">
+              <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['title']; ?>" />
+            </div>
+          <?php endforeach; ?>
+        <?php endif; ?>
       </div>
       <div class="project-details-trigger">
         <button type="button" class="tcon tcon-plus--minusfold" aria-label="add item">
@@ -28,34 +26,23 @@
         </button>
       </div>
       <div class="project-details">
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus recusandae, repellendus sint harum consectetur earum! Ipsam iusto aliquam odit modi recusandae pariatur velit similique. Esse veritatis explicabo, illum atque. Tempora.</p>
+        <p><?php the_content(); ?></p>
       </div>
     </div>
     <div class="clearfix relative">
       <div class="project-name-year">
-        <span class="name">Name</span><br>
-        <span class="year">2013</span>
+        <span class="name"><?php the_title(); ?></span><br>
+        <span class="year"><?php the_field('year'); ?></span>
       </div>
       <div class="gallery-nav js-flickity" data-flickity-options='{ "cellAlign": "left", "contain": true, "asNavFor": ".gallery", "pageDots": false, "prevNextButtons": false }'>
-        <div class="gallery-cell">
-          <img src="assets/img/img.png" alt="Image" class="ignore-srcset">
-        </div>
-        <div class="gallery-cell">
-          <img src="assets/img/img.png" alt="Image" class="ignore-srcset">
-        </div>
-        <div class="gallery-cell">
-          <img src="assets/img/img.png" alt="Image" class="ignore-srcset">
-        </div>
         <div class="gallery-cell">
           <img src="assets/img/img.png" alt="Image" class="ignore-srcset">
         </div>
       </div>
       <div class="gallery-counter js-flickity" data-flickity-options='{ "asNavFor": ".gallery", "pageDots": false, "prevNextButtons": false}'>
         <div class="gallery-cell"></div>
-        <div class="gallery-cell"></div>
-        <div class="gallery-cell"></div>
-        <div class="gallery-cell"></div>
       </div>
     </div>
+    <?php endwhile; endif; ?>
   </main>
 <?php get_footer(); ?>
