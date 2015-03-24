3a07eb604955084cd667bf9db4e35c56b033658d
Gus <yo@soygus.com>
2015-03-24 11:20:20 -0500

    Home

--- wp-content/themes/fierroles/index.php
+++ wp-content/themes/fierroles/index.php
@@ -5,7 +5,7 @@
 ?>
 
 <?php get_header(); ?>
-  <main class="main-content minhp-100 flex-middle-center" role="main">
+  <main class="minhp-100 flex-middle-center" role="main">
     <h1 class="h4 uppercase text-gray-dark lintel-bold m-0">Carlos Fierro</h1>
     <nav class="ph-ch-21">
       <a href="#">EN</a>
