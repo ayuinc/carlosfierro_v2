cdf00555312f935217b92ae513b71a17613f6427
Gus <yo@soygus.com>
2015-03-24 19:42:57 -0500

    Splash text

--- wp-content/themes/fierroles/index.php
+++ wp-content/themes/fierroles/index.php
@@ -5,9 +5,9 @@
 ?>
 
 <?php get_header(); ?>
-  <main class="minhp-90 flex-middle-center" role="main">
+  <main class="minhp-90 flex-middle-center normalize-text" role="main">
     <h1 class="h3 uppercase text-gray-darker lintel-bold m-0">Carlos Fierro</h1>
-    <nav class="ph-ch-21 pt-21 lintel-bold h4">
+    <nav class="ph-ch-21 pt-21 lintel-regular h3">
       <a href="<?php echo site_url(); ?>?page_id=4">EN</a>
       <a href="<?php echo site_url(); ?>?page_id=4">ES</a>
     </nav>
