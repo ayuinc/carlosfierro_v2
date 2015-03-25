15d880a56124abe84396798aa51e371e6f52b586
Gus <yo@soygus.com>
2015-03-24 19:39:30 -0500

    Splash

--- wp-content/themes/fierroles/index.php
+++ wp-content/themes/fierroles/index.php
@@ -6,10 +6,10 @@
 
 <?php get_header(); ?>
   <main class="minhp-90 flex-middle-center" role="main">
-    <h1 class="h4 uppercase text-gray-dark lintel-bold m-0">Carlos Fierro</h1>
-    <nav class="ph-ch-21 pt-21">
-      <a href="#">EN</a>
-      <a href="#">ES</a>
+    <h1 class="h3 uppercase text-gray-darker lintel-bold m-0">Carlos Fierro</h1>
+    <nav class="ph-ch-21 pt-21 lintel-bold h4">
+      <a href="<?php echo site_url()?page_id=4 ?>">EN</a>
+      <a href="<?php echo site_url()?page_id=4 ?>">ES</a>
     </nav>
   </main>
 <?php get_footer(); ?>
