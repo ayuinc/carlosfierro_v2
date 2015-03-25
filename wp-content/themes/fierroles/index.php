020bc820f5d99c866b72a81718dd05d7a7a67905
Gus <yo@soygus.com>
2015-03-24 19:41:08 -0500

    Splash layout

--- wp-content/themes/fierroles/index.php
+++ wp-content/themes/fierroles/index.php
@@ -8,8 +8,8 @@
   <main class="minhp-90 flex-middle-center" role="main">
     <h1 class="h3 uppercase text-gray-darker lintel-bold m-0">Carlos Fierro</h1>
     <nav class="ph-ch-21 pt-21 lintel-bold h4">
-      <a href="<?php echo site_url()?page_id=4 ?>">EN</a>
-      <a href="<?php echo site_url()?page_id=4 ?>">ES</a>
+      <a href="<?php echo site_url(); ?>?page_id=4">EN</a>
+      <a href="<?php echo site_url(); ?>?page_id=4">ES</a>
     </nav>
   </main>
 <?php get_footer(); ?>
