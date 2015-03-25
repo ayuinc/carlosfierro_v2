444f6aad7c7246759f4cf28f0c89ccbfd5e2fedb
Gus <yo@soygus.com>
2015-03-25 18:12:07 -0500

    Heights

--- wp-content/themes/fierroles/single-works.php
+++ wp-content/themes/fierroles/single-works.php
@@ -8,7 +8,7 @@
           $images = get_field('gallery');
           if( $images ): ?>
           <?php foreach( $images as $image ): ?>
-            <div class="gallery-cell bg-img-block" style="background-image: url(<?php echo $image['url']; ?>)">
+            <div class="gallery-cell bg-img-block bg-img-block-top-y" style="background-image: url(<?php echo $image['url']; ?>)">
               <!-- <img src="<?php echo $image['url']; ?>" alt="<?php echo $image['title']; ?>" /> -->
             </div>
           <?php endforeach; ?>
