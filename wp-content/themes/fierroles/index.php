0afbd092ca3161cdd485bf872036341a6ee11c34
Gus <yo@soygus.com>
2015-03-31 12:57:31 -0500

    ES translate

--- wp-content/themes/fierroles/index.php
+++ wp-content/themes/fierroles/index.php
@@ -9,7 +9,7 @@
     <h1 class="h3 uppercase text-gray-darker lintel-bold m-0">Carlos Fierro</h1>
     <nav class="ph-ch-21 pt-21 lintel-regular h3">
       <a href="<?php echo site_url(); ?>?page_id=4">EN</a>
-      <a href="<?php echo site_url(); ?>?page_id=4">ES</a>
+      <a href="<?php echo site_url(); ?>?page_id=80&lang=es">ES</a>
     </nav>
   </main>
 <?php get_footer(); ?>
