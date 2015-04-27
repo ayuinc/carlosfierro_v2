fb5b313da355ae17d1dea73db0738289d8f9577b
Gus <yo@soygus.com>
2015-04-27 17:15:58 -0500

    Flex home update

--- wp-content/themes/fierroles/index.php
+++ wp-content/themes/fierroles/index.php
@@ -5,7 +5,7 @@
 ?>
 
 <?php get_header(); ?>
-  <main class="minhp-90 flex-middle-center normalize-text" role="main">
+  <main class="minhp-90 flex-column-center flex-align-center normalize-text" role="main">
     <h1 class="h3 uppercase text-gray-darker lintel-bold m-0">Carlos Fierro</h1>
     <nav class="ph-ch-21 pt-21 lintel-regular h3">
       <a href="<?php echo site_url(); ?>?page_id=4">EN</a>
