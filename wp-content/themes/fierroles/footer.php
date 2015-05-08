e0a6702b76255f7d0267bf72703167578f4025fa
Gus <yo@soygus.com>
2015-05-08 18:27:17 -0500

    Issue #16

--- wp-content/themes/fierroles/footer.php
+++ wp-content/themes/fierroles/footer.php
@@ -1,5 +1,6 @@
 				</div> <!-- END:Site content -->
 			</div> <!-- END:Site wrapper -->
+		<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-52f279493184b19f" async="async"></script>
 		<?php wp_footer(); ?>
 	</body>
 </html>
