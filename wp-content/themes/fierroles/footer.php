499def1ffc6fd081252946d7d393238948b35691
Gus <yo@soygus.com>
2015-05-08 19:04:37 -0500

    Add this

--- wp-content/themes/fierroles/footer.php
+++ wp-content/themes/fierroles/footer.php
@@ -1,6 +1,6 @@
 				</div> <!-- END:Site content -->
 			</div> <!-- END:Site wrapper -->
-		<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-52f279493184b19f" async="async"></script>
+		<script type="text/javascript" src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-554d4ec0747cd7e0" async="async"></script>
 		<?php wp_footer(); ?>
 	</body>
 </html>
